443e04497ef08165485150463454ed61af350dc8
KKalamees <karmo.kalamees@gmail.com>
2024-09-14 15:41:34 +0300

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//rakendus küsib kasutajalt valida tema sugu
+//rakenus küsib kasutajalt perekonnanime
+//lähtudes kasutaja valikust , rakendus tervitab teda järgmiselt
+//"Welcome, Mr. (perekonnanimi)" /"Welcome, Ms.(perekonnanimi)"
+
+
+Console.WriteLine("Please, select your gender (m/f)");
+char userGender = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Please, enter your  last name");
+string userSurname = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userSurname}!");
+}
+
+else  if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userSurname}!");
+}
+
+else
+{
+    Console.WriteLine($"Welcome, {userSurname}!");
+}
---
+++ UserAgeValidation2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//rakendus küsib kasutajalt sisestada tema vanuse
+//kui kasutaja on noorem kui 13; siis konsoolis kuvatakse:
+//"You are too young for this app"
+//muul juhul kuvatakse "Welcome"
+
+Console.WriteLine("Enter your age:");
+
+string userAge = Console.ReadLine();
+int userAgeNum = 0;
+
+bool isAgeNumber = Int32.TryParse(userAge,  out userAgeNum);
+
+
+if (isAgeNumber == true)
+{
+    if (userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome");
+    }
+    else
+    {
+        Console.WriteLine("You are too young for this app");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age");
+
+}
